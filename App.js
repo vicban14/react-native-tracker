aa3bccfe19e54023fa8c1746df04f16d677c290f
Víctor Bañuls <vicbansax@gmail.com>
2020-08-28 14:44:12 +0200

    Add contexts provider

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+
 import SignupScreen from './src/screens/SignupScreen'
 import SigninScreen from './src/screens/SigninScreen'
 import TrackListScreen from './src/screens/TrackListScreen'
 import TrackDetailScreen from './src/screens/TrackDetailScreen'
 import TrackCreateScreen from './src/screens/TrackCreateScreen'
 import AccountScreen from './src/screens/AccountSreen'
+import { Provider as AuthProvider } from './src/context/AuthContext'
 
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
@@ -24,4 +26,12 @@ const switchNavigator = createSwitchNavigator({
   }),
 })
 
-export default createAppContainer(switchNavigator)
+const App = createAppContainer(switchNavigator)
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  )
+}
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,26 +1,57 @@
-import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Text, Button, Input } from 'react-native-elements'
+import Spacer from '../components/Spacer'
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <View>
-      <Text>Signup Screen</Text>
-      <Button
-        title='Go to Signin'
-        onPress={() => {
-          navigation.navigate('Signin')
-        }}
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign up for Tracker</Text>
+      </Spacer>
+      <Input
+        label='Email'
+        value={email}
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize='none'
+        autoCorrect={false}
       />
-      <Button
-        title='Go to Main Flow'
-        onPress={() => {
-          navigation.navigate('TrackList')
-        }}
+      <Spacer />
+      <Input
+        secureTextEntry
+        label='Password'
+        value={password}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+        autoCapitalize='none'
+        autoCorrect={false}
       />
+      <Spacer>
+        <Button
+          title='Go to Signin'
+          onPress={() => {
+            navigation.navigate('Signin')
+          }}
+        />
+      </Spacer>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  },
+})
 
 export default SignupScreen
